5a88a0f0267d107870e0444cf33d8299d1fd4587
Holly Phillips <hooleymcknight.codes@gmail.com>
2024-09-09 14:58:04 -0700

    emptied main

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Overlay from './Overlay/Overlay';
-
-ReactDOM.render(
-  <Overlay />,
-  document.getElementById('root')
-)
